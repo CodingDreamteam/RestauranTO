90b5f2b70f5319dd6bd2e73f93e52071fe914efd
Juan Manuel <Juan Manuel@192.168.1.100>
2017-10-23 21:15:07 -0400

    advanced search things

--- RestauranTO/src/main/java/RestauranTO/controller/advancedsearch/CAdvancedSearchController.java
+++ RestauranTO/src/main/java/RestauranTO/controller/advancedsearch/CAdvancedSearchController.java
@@ -1,15 +1,29 @@
 package RestauranTO.controller.advancedsearch;
 
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Textbox;
 
 
 public class CAdvancedSearchController extends SelectorComposer<Component> {
 
     private static final long serialVersionUID = 4539859646034119338L;
     
+    @Wire
+    Label labelzonesearch;
+    @Wire
+    Textbox search;
     
-    
-    
-    
+    @Listen( "onClick=#buttonSearch" )
+    public void onClickButtonSearch( Event event ) {
+
+       
+        
+        
+        
+    }    
 }
